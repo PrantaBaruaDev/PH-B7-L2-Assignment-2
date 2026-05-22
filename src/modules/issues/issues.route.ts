f3919bab1d8213681af1cd89170ffac6d21aab48
Pranta <baruapranta765@gmail.com>
2026-05-22 23:21:14 +0600

    Add - Build and deploy packages

--- src/modules/issues/issues.route.ts
+++ src/modules/issues/issues.route.ts
@@ -5,9 +5,9 @@ import auth from "../../middleware/auth";
 
 const router = Router();
 
+router.get("/", issuesController.getIssues);
+router.get("/:id", issuesController.getSingleIssue);
 router.post("/", auth(USER_ROLE[0], USER_ROLE[1]), issuesController.createIssues);
-router.get("/", auth(USER_ROLE[0], USER_ROLE[1]), issuesController.getIssues);
-router.get("/:id", auth(USER_ROLE[0], USER_ROLE[1]), issuesController.getSingleIssue);
 router.patch("/:id", auth(USER_ROLE[0], USER_ROLE[1]), issuesController.updateIssues);
 router.delete("/:id", auth(USER_ROLE[1]), issuesController.deleteIssues); // only maintainer
 
